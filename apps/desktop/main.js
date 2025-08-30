c2a2ef7928d98f8898f19c20e9459639c2a0ad51
Dev Patel <devptl841806@gmail.com>
2025-08-30 16:18:34 -0400

    electron top level wrapper

--- apps/desktop/main.js
+++ apps/desktop/main.js
@@ -1,15 +1,19 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
+const { app, BrowserWindow } = require("electron");
 
 function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
+      nodeIntegration: false,
+    },
   });
-  win.loadURL('http://localhost:3000');
+
+  win.loadURL("http://localhost:3000");
 }
 
 app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
