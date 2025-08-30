6aae5f278211668bf0646052e4006b0092a8a223
Dev Patel <devptl841806@gmail.com>
2025-08-30 17:22:54 -0400

    transparent / glassy background on electron start

--- apps/desktop/main.js
+++ apps/desktop/main.js
@@ -1,19 +1,33 @@
 const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+const isDev = !app.isPackaged;
 
 function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
+    transparent: true,
+    frame: false,
+    vibrancy: "fullscreen-ui",
+    visualEffectState: "active",
+    backgroundColor: "#00000000",
     webPreferences: {
       nodeIntegration: false,
+      contextIsolation: true,
     },
   });
 
-  win.loadURL("http://localhost:3000");
+  if (isDev) {
+    const devUrl = "http://localhost:3000";
+    console.log("Loading dev URL:", devUrl);
+    win.loadURL(devUrl);
+    win.webContents.openDevTools();
+  } else {
+    const prodPath = path.join(__dirname, "../web/out/index.html");
+    console.log("Loading prod file:", prodPath);
+    win.loadFile(prodPath);
+  }
 }
 
 app.whenReady().then(createWindow);
-
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") app.quit();
-});
