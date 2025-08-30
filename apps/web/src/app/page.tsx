61e68ec090ecb2e2f1aa10adbaf0251681efec1f
Dev Patel <devptl841806@gmail.com>
2025-08-30 18:19:47 -0400

    removed sample logos

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -3,7 +3,6 @@
 export default function Home() {
   return (
     <div className="frosty flex flex-col min-h-screen">
-      {/* Draggable Top Bar */}
       <header className="drag h-10 w-full flex items-center justify-between px-4">
         <span className="text-white/70 font-semibold">Everything Dashboard</span>
       
@@ -11,7 +10,9 @@ export default function Home() {
 
       {/* Content */}
       <main className="flex-1 flex items-center justify-center text-white">
-        <h1 className="text-2xl font-regular">Everything.</h1>
+        <div className="inline-flex text-center items-center gap-4">
+                  <h1 className="font-regular text-2xl">Everything Dashboard</h1>
+        </div>
       </main>
     </div>
   );
